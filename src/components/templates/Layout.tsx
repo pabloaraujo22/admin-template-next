e0f476027ab8af8f956e3750a822535b882eb42e
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-04-23 16:29:15 -0300

    Esquema de Cores

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -11,7 +11,9 @@ export default function Layout(props: LayoutProps) {
     return (
         <div className={`flex h-screen w-screen`}>
             <MenuLateral />
-            <div className={`flex flex-col w-full p-7 bg-gray-300`}>
+            <div
+                className={`flex flex-col w-full p-7 bg-gray-300 dark:bg-gray-800`}
+            >
                 <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
                 <Conteudo>{props.children}</Conteudo>
             </div>
