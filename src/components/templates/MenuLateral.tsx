a472641a141eae7e24b30e8cf1239c2e5dfd2bd4
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-04-23 11:51:07 -0300

    Estrutura Inicial

---
+++ src/components/templates/MenuLateral.tsx
@@ -0,0 +1,8 @@
+interface MenuLateralProps {}
+export default function MenuLateral(props: MenuLateralProps) {
+    return (
+        <aside>
+            <h1>Menu</h1>
+        </aside>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import Layout from '../components/templates/Layout';
+
 export default function Home() {
     return (
-        <div className="flex justify-center items-center h-screen bg-gradient-to-r from-green-500 to-blue-500">
-            Admin
-        </div>
+        <Layout
+            titulo="Página Inicial"
+            subtitulo="Estamos construindo um template admin"
+        >
+            <h3>Conteúdo</h3>
+        </Layout>
     );
 }
