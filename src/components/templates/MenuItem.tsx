1cf4095f7170f428d889775d191b872dd6cef8a3
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-04-23 16:38:54 -0300

    Componente Menu Lateral 1

---
+++ src/components/templates/MenuItem.tsx
@@ -0,0 +1,8 @@
+interface MenuItemProps {
+    url: string;
+    texto: string;
+    icone: any;
+}
+export default function MenuItem(props: MenuItemProps) {
+    return <li className={``}>{props.icone}</li>;
+}
--- src/components/templates/MenuLateral.tsx
+++ src/components/templates/MenuLateral.tsx
@@ -1,8 +1,23 @@
+import { IconeAjustes, IconeCasa, IconeSino } from '../icons';
+import MenuItem from './MenuItem';
+
 interface MenuLateralProps {}
 export default function MenuLateral(props: MenuLateralProps) {
     return (
         <aside>
-            <h1>Menu</h1>
+            <ul>
+                <MenuItem url="/" texto="Inicio" icone={<IconeCasa />} />
+                <MenuItem
+                    url="/ajustes"
+                    texto="Ajustes"
+                    icone={<IconeAjustes />}
+                />
+                <MenuItem
+                    url="/notificacoes"
+                    texto="Notificações"
+                    icone={<IconeSino />}
+                />
+            </ul>
         </aside>
     );
 }
