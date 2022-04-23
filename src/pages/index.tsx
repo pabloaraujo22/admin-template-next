46426357c16f8ffae434c08508e8d27ddd9f2b79
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-04-23 11:37:42 -0300

    Configuração Inicial do Projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex justify-center items-center h-screen bg-gradient-to-r from-green-500 to-blue-500">
+            Admin
+        </div>
+    );
+}
